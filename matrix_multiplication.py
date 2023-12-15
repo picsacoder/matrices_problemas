62694000c293cfda352454443c355a6d6e689531
picsacoder <omarsaidortiz09@gmail.com>
2023-12-15 11:42:33 -0600

    multiply matrix nxn

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -1,4 +1,13 @@
 def matrix_mult(a, b):
-  # your code here
-  return
+  r = []
+  
+  for i in range(0,len(a)): #len(a) = 2, rows
+    r.append([])
+    for j in range(0,len(b[0])): 
+      r[i].append(0)
+      for k in range(0,len(b)):
+        r[i][j] += a[i][k] * b[k][j]
+  return r
+
+matrix_mult([[1,2],[3,2]],[[3,2],[1,1]])
 
